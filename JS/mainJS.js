d9486c6957b5e9d6fe3d9c1068c97b626229e839
msn02 <mistyshainesambajon.niones@bicol-u.edu.ph>
2023-04-30 18:31:22 +0800

    added functionality for navigation bars

--- JS/mainJS.js
+++ JS/mainJS.js
@@ -1,7 +1,5 @@
-const activePage = window.location.pathname;
-const navLink = document.querySelectorAll('nav a').forEach(link => {
-	if (link.href.inludes('${activePage}')) {
-		link.classlist.add('active');
-		// to be edited later
+document.querySelectorAll('.navlink').forEach(link=>{
+	if (link.href===window.location.href){
+		link.setAttribute('aria-current','page');
 	}
-})
+});
