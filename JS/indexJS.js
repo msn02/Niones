a4166986011c108848345dfb99c2839c344ff102
msn02 <mistyshainesambajon.niones@bicol-u.edu.ph>
2023-04-30 18:27:22 +0800

    modified functionality for log-in

--- JS/indexJS.js
+++ JS/indexJS.js
@@ -1,23 +1,39 @@
 const form = document.getElementById('form');
+const uname = document.getElementById("uname");
+const pass = document.getElementById("pass");
+const unameM = document.getElementById("unameM");
+const passM = document.getElementById("passM");
+
+const textStyle1 = document.querySelector('#unameM');
+const textStyle2 = document.querySelector('#passM');
+
+// log in credentials
+var username = "admin";
+var password = "password123";
 
 form.addEventListener('submit', e => {
 	e.preventDefault();
 });
 
 function inputChecker() {
-	var uname = document.getElementById("uname").value;
-	var password = document.getElementById("pass").value;
-	let unameM = document.getElementById("unameM");
-	let passM = document.getElementById("passM");
-
-	if(uname=="admin") {
-		if (password=="pasok123") {
-			window.location.assign("aboutMe.html");
-		} else {
-			// change the text and color into red for the password
-		}
+	if (uname.value.trim()==username && pass.value.trim()==password) {
+		window.location.assign("aboutMe.html");
 	} else {
-		// for the username, change the text and color to red
-		return;
+		// username
+		if (uname.value.trim()==username) {
+			textStyle1.style.setProperty('opacity', '0');
+		} 
+		else {
+			textStyle1.style.setProperty('color', 'red');
+			unameM.innerHTML = "Invalid username";
+		}
+		// password
+		if (pass.value.trim()==password) {
+			textStyle2.style.setProperty('opacity', '0');
+		} 
+		else {
+			textStyle2.style.setProperty('color', 'red');
+			passM.innerHTML = "Invalid password"; 
+		}
 	}
 };
