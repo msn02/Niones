2f46e1f2ef95af3413fcbcb49871c76573d28157
msn02 <mistyshainesambajon.niones@bicol-u.edu.ph>
2023-05-20 01:41:10 +0800

    modified target class

--- JS/mainJS.js
+++ JS/mainJS.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.navlink').forEach(link=>{
+document.querySelectorAll('.nav-link').forEach(link=>{
 	if (link.href===window.location.href){
 		link.setAttribute('aria-current','page');
 	}
