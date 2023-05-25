fc81715528d51c945a96861446554277c6ff5682
msn02 <mistyshainesambajon.niones@bicol-u.edu.ph>
2023-05-26 01:22:27 +0800

    reusable navbar

---
+++ Others/navbar.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<nav class="navbar navbar-expand-md mt-1">
+	<div class="container-fluid mx-2">
+		<div class="navbar-brand">
+			<img src="Images/logoBlack.png" class="imageSize img-fluid" alt="logo">
+		</div>
+		<!-- Custom toggler button -->
+	    <button class="navbar-toggler btn shadow-none border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarLink" aria-controls="navbarLink" aria-expanded="false" aria-label="Toggler">
+	    	<div>
+	    		<div class="togglerBorder"></div>
+		    	<div class="togglerBorder"></div>
+		    	<div class="togglerBorder"></div>
+	    	</div>
+	    </button>
+		<!-- Container for the 3 links & Log out button -->
+		<div class="collapse navbar-collapse" id="navbarLink">
+			<ul class="navText navbar-nav ms-auto">
+				<li class="nav-item">
+    				<a class="nav-link" href="aboutMe.php">About me</a>
+    			</li>
+				<li class="nav-item">
+    				<a class="nav-link" href="portfolio.php">Portfolio</a>
+    			</li>
+				<li class="nav-item">
+    				<a class="nav-link" href="futurePlans.php">Future plans</a>
+    			</li>        			
+			</ul>
+			<div class="contactbutton py-3">
+				<a href="contactMe.php"><button class="btn shadow-sm border-0 rounded-1" type="button">
+				Contact Me</button></a>
+			</div>
+		</div>
+	</div>
+</nav>
+</body>
+</html>
